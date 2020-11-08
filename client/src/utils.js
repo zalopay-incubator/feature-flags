fc11834b34011de3803fb4ea0b4a6096d85fcace
ozuit <tantd2@vng.com.vn>
2020-11-08 21:37:53 +0700

    Bundle client app by Docker

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,7 +1,7 @@
 /** Make a server call to get all registered feature flags.  */
 export async function getFeatures() {
   try {
-    const response = await fetch('http://localhost:4242/features?uid=123')
+    const response = await fetch(`${process.env.REACT_APP_UNLEASH_API}?uid=123`)
     return response.json()
   } catch (error) {
     return {}
